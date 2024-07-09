d14a79afe7389504ba14fd3f191cd0a3430599c9
jiin <jiin.kim109@gmail.com>
2024-07-09 13:21:03 -0700

    foo: remove jest.components.config

--- jest.components.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'jest-expo',
-    setupFilesAfterEnv: ['@testing-library/react-native/extend-expect', './src/components/__tests__/setupTest.ts'],
-    transform: {
-        '^.+\\.(js|ts|tsx)$': 'babel-jest',
-    },
-    testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '@react-native'],
-};
