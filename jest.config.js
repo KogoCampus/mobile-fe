dcd4fafe874b37da0db13525dd24ca2a2c927b83
Jiin Kim <jiinkim@Jiins-MacBook-Pro.local>
2024-10-07 12:38:09 -0400

    foo: resolve test failure

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const config = {
     projects: [
         {
@@ -12,6 +14,9 @@ const config = {
                 '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ttf)$': '<rootDir>src/components/__tests__/__mocks__/fileMock.ts',
                 '\\.(css|less|scss|sass)$': '<rootDir>/src/components/__tests__/__mocks__/styleMock.ts',
             },
+            transformIgnorePatterns: [
+              "node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))"
+            ]
         },
     ],
 };
